1ec034d23e7d88b5d40bbdbabd2756e6ff901d89
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-19 12:24:57 +0430

    closures  added.

---
+++ 11_closures/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func adder() func(int) int {
+	sum := 0
+	return func(x int) int {
+		sum += x
+		return sum
+	}
+}
+
+func main() {
+	sum := adder()
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%d : %d\n\n", i, sum(i))
+	}
+}
