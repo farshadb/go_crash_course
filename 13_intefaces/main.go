db26bced3e137adc285c4f0bd0a22e8793804a85
F4r5h4d <bolouri.farshad@gmail.com>
2022-07-19 13:22:13 +0430

    intefaces  added.

---
+++ 13_intefaces/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+// Define interface
+type Shape interface {
+	area() float64
+}
+
+type Circle struct {
+	x, y, radius float64
+}
+
+type Rectangle struct {
+	width, height float64
+}
+
+func (c Circle) area() float64 {
+	return math.Pi * c.radius * c.radius
+}
+func (r Rectangle) area() float64 {
+	return r.width * r.height
+}
+
+func getArea(s Shape) float64 {
+	return s.area()
+}
+
+func main() {
+	circle := Circle{x: 0, y: 0, radius: 5}
+	rectangle := Rectangle{width: 5, height: 10}
+
+	fmt.Println("Circle Area is: ", getArea(circle))
+	fmt.Println("Rectangle Area is: ", rectangle.area())
+
+}
